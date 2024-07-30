55bf502c9ea5b3d41898966906fef892c952aaa1
Ismat <ismetsemedov@gmail.com>
2024-07-30 14:13:46 +0400

    added

--- venv/lib/python3.12/site-packages/kaggle/__init__.py
+++
@@ -1,7 +0,0 @@
-# coding=utf-8
-from __future__ import absolute_import
-from kaggle.api.kaggle_api_extended import KaggleApi
-from kaggle.api_client import ApiClient
-
-api = KaggleApi(ApiClient())
-api.authenticate()
