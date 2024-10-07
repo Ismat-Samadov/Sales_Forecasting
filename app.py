41e22cf2c7a328b8a29ada8b666b0cda180f9f25
Ismat <ismetsemedov@gmail.com>
2024-10-07 18:35:03 +0400

    check

--- app.py
+++ app.py
@@ -87,5 +87,6 @@ def predict():
     # Return JSON response
     return jsonify({'chart': chart_url, 'sales': sorted_sales_predictions})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
